61bd711dee7e93b2491a1621ea07fc68e2e0091c
2lu3 <hi2lu3@gmail.com>
2026-05-08 15:53:10 +0900

    dbに接続できた
    rest apiで接続したが、cloudflare workerを使うことにした

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -3,6 +3,7 @@ from fastapi import UploadFile, File, HTTPException
 from pydantic import BaseModel
 import boto3
 import uuid
+from cloudflare import Cloudflare
 
 
 class CreateUploadRequest(BaseModel):
@@ -10,17 +11,24 @@ class CreateUploadRequest(BaseModel):
     size: int
     content_type: str
 
-from dotenv import load_dotenv
-load_dotenv()
-from os import environ
 
-s3 = boto3.client('s3',
-endpoint_url=environ.get('AWS_ENDPOINT_URL'),
-aws_access_key_id=environ.get('AWS_ACCESS_KEY_ID'),
-aws_secret_access_key=environ.get('AWS_SECRET_ACCESS_KEY'),
-region_name='auto',
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=os.environ.get("AWS_ENDPOINT_URL"),
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    region_name="auto",
 )
+client = Cloudflare()
+
+
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -31,6 +39,7 @@ def read_root():
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/uploads")
 def upload_file(body: CreateUploadRequest):
     filename = body.filename
@@ -38,20 +47,40 @@ def upload_file(body: CreateUploadRequest):
     content_type = body.content_type
 
     if content_type != "application/pdf":
-        raise HTTPException(status_code=400, detail="Content type must be application/pdf")
+        raise HTTPException(
+            status_code=400, detail="Content type must be application/pdf"
+        )
     if size > 100 * 1024 * 1024:
         raise HTTPException(status_code=400, detail="File size must be less than 100MB")
 
     s3_key = str(uuid.uuid4())
     post_url = s3.generate_presigned_url(
-        'put_object',
+        "put_object",
         Params={
-            'Bucket': environ.get('AWS_BUCKET_NAME'),
-            'Key': s3_key,
-            'ContentType': content_type,
+            "Bucket": environ.get("AWS_BUCKET_NAME"),
+            "Key": s3_key,
+            "ContentType": content_type,
         },
-        ExpiresIn=60, # seconds
+        ExpiresIn=60,  # seconds
     )
+
+
+    d1 = client.d1.database.get(
+        database_id=os.environ.get("CLOUDFLARE_D1_DATABASE_ID"),
+        account_id=os.environ.get("CLOUDFLARE_ACCOUNT_ID"),
+    )
+
+    client.d1.database.query(
+        database_id=os.environ.get("CLOUDFLARE_D1_DATABASE_ID"),
+        account_id=os.environ.get("CLOUDFLARE_ACCOUNT_ID"),
+        data={
+            "filename": filename,
+            "size": size,
+            "content_type": content_type,
+        },
+    )
+    
+
 
     # save db followings:
     # - filename
